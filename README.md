a206de28ac1e0e50bdf833f7a1c04ef122553a65
kangfend <kangfend@gmail.com>
2014-08-17 16:35:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+py-md5crack
+===========
+
+This tools can crack md5 hash over http://hashkiller.co.uk
+
+Installation
+============
+1. Install requirement:
+
+    `sudo apt-get install python-imaging python-mechanize python-pyquery tesseract-ocr`
+
+2. Install py-md5crack:
+
+    `sudo pip install https://github.com/kangfend/py-md5crack/archive/master.zip`
+
+Command
+=======
+You can use this tool with this command `md5crack [hash]`
+
+example: `md5crack 4dd39f49f898c062283963c187532af8`
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  py-md5crack - Crack your md5 hash over http://haskiller.co.uk
+#  Copyright (C) 2014 Sutrisno Efendi - kangfend@gmail.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "py-md5crack",
+    version = "0.1.0",
+    url = 'https://github.com/kangfend/py-md5crack',
+    description = 'Crack your md5 hash over http://haskiller.co.uk',
+    license = 'GNU/GPL',
+    author = 'Sutrisno Efendi',
+    author_email = 'kangfend@gmail.com',
+    packages = find_packages(),
+    install_requires = ['pyquery', 'mechanize', 'PIL'],
+    scripts=['bin/md5crack'],
+)
